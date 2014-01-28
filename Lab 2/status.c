4b8899258a44c3b7e982bfcab7b7d4267633b2bd
Paul Bartell <paul.bartell@gmail.com>
2014-01-28 12:32:58 -0800

    Changed status to use global counter

--- Lab 2/status.c
+++ Lab 2/status.c
@@ -1,9 +1,14 @@
 #include "tasks.h"
+#include "schedule.h"
+
+extern unsigned long globaltime;
 
 void status(void* taskDataPtr)
 {
+  if(globaltime % MAJORCYCLECOUNT == 0)
+  {
     // Pointer re-cast as pointer to the Status task's data structure
     StatusData* statusDataPtr = (StatusData*) taskDataPtr;
-
     (*(statusDataPtr->batteryState))--;
+  }
 }
